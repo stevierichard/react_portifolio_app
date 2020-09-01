b5f781122c173a5cba8e0e2b045e1fe4c45b4a3b
Steven Simon <stevierichard1@gmail.com>
2020-09-01 13:17:58 -0700

    change home page profile

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -8,7 +8,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 // import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import OfficePic from "../../images/office img.jpg";
+import OfficePic from "../../images/Ubalozi.jpg";
 
 function Copyright() {
   return (
